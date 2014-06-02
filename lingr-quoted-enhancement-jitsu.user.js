dafaa84293ac395af7770b453ec23eeb0f3ce8d1
Code Ass <aycabta@gmail.com>
2014-06-02 11:37:50 +0900

    fix archive page logic

--- lingr-quoted-enhancement-jitsu.user.js
+++ lingr-quoted-enhancement-jitsu.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name       Lingr quoted enhancement jitsu
 // @namespace  http://aycabta.github.io/
-// @version    0.1.4
+// @version    0.1.5
 // @description  The Benry Script
 // @include    http://lingr.com/
-// @include    http://lingr.com/room/censored/archives*
+// @include    http://lingr.com/room/*/archives*
 // @copyright  2013+, Code Ass
 // ==/UserScript==
 
@@ -18,16 +18,15 @@
             }
             newStr = Lingr.Text.oldLingrQuotedEnhancementJitsuDecorate(str);
             return newStr.split('\n').map(function(s) {
-                return s.replace(/(^|<p>)((?:&gt;|ï¼ž).*?)($|<\/p>)/g, '$1<span class="quoted">$2</span>$3');
+                return s.replace(/(^|<p>)((?:&gt;|¡ä).*?)($|<\/p>)/g, '$1<span class="quoted">$2</span>$3');
             });
         };
     } else {
         var i;
         var messages = $("div.decorated p");
         for (i = 0; i < messages.length; i++) {
-            var oldMessage = messages[i].innerHTML;
-            messages[i].innerHTML = oldMessage.replace(/^((?:&gt;|ï¼ž).*?)$/g, '<span class="quoted">$1</span>');
-            console.log(messages[i].innerHTML);
+            var message = messages[i];
+            message.innerHTML = message.innerHTML.replace(/^((?:&gt;|¡ä).*?)$/g, '<span class="quoted">$1</span>');
         }
     }
     var style = document.createElement('style');
